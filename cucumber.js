5b9f9679ca6b5be25285a6373a0836366d6cf772
KylieNamugga <96869130+KylieNamugga@users.noreply.github.com>
2022-06-13 21:16:50 +0300

    main files

---
+++ cucumber.js
@@ -0,0 +1,6 @@
+module.exports = {
+    default:  `--format-options '{"snippetInterface": "synchronous"}',`
+    
+  }
+
+  module.exports = { default: '--publish-quiet' } 
---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+function totalAmountSpent(amount){
+    return 100
+}
+
+Given('I buy a pair of shoes for ${int}', function (int) {
+    this.amount = int;
+});
+
+When('I pay for them I get a receipt with the total amount', function () {
+    this.actualAmount = totalAmountSpent(this.amount);
+    // return 'pending';
+});
+
+Then('I should see the total amount spent is ${int}', function (int) {
+    assert.strictEqual(this.actualAmount, int);
+    // return 'pending';
+});
