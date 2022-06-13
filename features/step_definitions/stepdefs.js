1773b98d31af7d42b1006263965b55f82e13ab8f
KylieNamugga <96869130+KylieNamugga@users.noreply.github.com>
2022-06-13 21:19:49 +0300

    done

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -11,10 +11,8 @@ Given('I buy a pair of shoes for ${int}', function (int) {
 
 When('I pay for them I get a receipt with the total amount', function () {
     this.actualAmount = totalAmountSpent(this.amount);
-    // return 'pending';
 });
 
 Then('I should see the total amount spent is ${int}', function (int) {
     assert.strictEqual(this.actualAmount, int);
-    // return 'pending';
 });
